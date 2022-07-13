2a4a19498fac66da08862794cf5796e0439518d0
Jiří Ludvík <jludvik@eprin.cz>
2022-07-13 22:27:32 +0200

    Added enter and delete events to input component

--- src/components/inputField.jsx
+++ src/components/inputField.jsx
@@ -3,24 +3,37 @@ import './css/list.css'
 
 class InputField extends Component {
     state = { 
-        postText: ""
-     }
-
-     handleInput = event => {
-        this.setState({ postText: event.target.value })
+        postText: "",
+        allowInstantDelete: false
      }
     
     render() { 
         return (
             <div>
-                <input type={"text"} onChange={this.handleInput} ref={(el) => this.postText = el} className="Input-inputField m-2"/>
-                <button className='btn btn-primary m-2' onClick={() => this.handleConfirm(this.state.postText)}>Add post!</button>
+                <input type={"text"} onChange={this.handleInput} ref={(el) => this.postText = el} onKeyDown={this.handleKeyDown} className="Input-inputField m-2"/>
+                <button className='btn btn-primary m-2' onClick={() => this.handleConfirm()}>Add post!</button>
                 <button className='btn btn-primary m-2' onClick={() => this.props.onDelete(this.props.id)}>Remove last post!</button>
+                <input className='Input-checkbox' type={"checkbox"} onChange={this.handleCheckBoxChanged}></input>
+                <label className='Input-label' >Allow instant delete (del)</label>
             </div>
         );
     }
 
-    handleConfirm(input){
+    handleCheckBoxChanged = event => {
+        this.setState({allowInstantDelete: event.target.checked})
+    }
+
+    handleKeyDown = event => {
+        if (event.key === 'Enter') this.handleConfirm();
+        if (event.key === 'Delete' && this.state.allowInstantDelete) this.props.onDelete(this.props.id);
+    }
+
+    handleInput = event => {
+        this.setState({ postText: event.target.value })
+     }
+
+    handleConfirm(){
+        const input = this.state.postText;
         if (input === ""){
             window.alert("Entry was empty!")
             return;
